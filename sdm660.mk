7f663bb0b5c7ccf3c7e7ed17afbebe5a8bc31bbc
Kunmun <kunmun.devroms@gmail.com>
2022-02-15 01:27:30 +0530

    sdm660-common: seccomp: Move to QTI common components
    Change-Id: I2ec14b6463ca51b2be6a16cf5619f7ff61926356
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>
    Signed-off-by: raiyanbinmohsin <raiyanbinmohsinshishir@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -435,9 +435,7 @@ PRODUCT_PACKAGES += \
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/seccomp/imsrtp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/imsrtp.policy \
-    $(COMMON_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(COMMON_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+    $(COMMON_PATH)/seccomp/imsrtp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/imsrtp.policy
 
 # Sensors
 PRODUCT_PACKAGES += \
